eed1a6d67de18d36e935330261a557dce3fccf97
Malachi Willey <malwilley@gmail.com>
2018-05-25 10:54:09 -0500

    Add HttpContent component

---
+++ src/components/HttpContent.tsx
@@ -0,0 +1,29 @@
+import { HttpRequest } from '../types';
+
+type HttpContentProps<T> = {
+  renderError: (message: string) => JSX.Element
+  renderFetching: () => JSX.Element
+  renderSuccess: (data: T) => JSX.Element
+  request: HttpRequest<T>
+};
+
+const HttpContent = <T extends object | number | string>({ 
+  request, 
+  renderError, 
+  renderFetching, 
+  renderSuccess 
+}: HttpContentProps<T>) => {
+  switch (request.type) {
+    case 'success':
+      return renderSuccess(request.data);
+    case 'error':
+      return renderError(request.message);
+    case 'fetching':
+      return renderFetching();
+    case 'not_asked':
+    default:
+      return null;
+  } 
+};
+
+export default HttpContent;
