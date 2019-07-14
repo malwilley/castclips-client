57a318dcee75259b97673d3f4dc8c0f5773bf738
Malachi Willey <malwilley@gmail.com>
2019-07-14 10:41:09 -0500

    Animate episode clip options show/hide

--- src/modules/episode/components/EpisodePlayerClipOptions.tsx
+++ src/modules/episode/components/EpisodePlayerClipOptions.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Show from 'src/components/Show';
 import ClipPreview from './ClipPreview';
 import TimeRecorder from './TimeRecorder';
@@ -9,7 +9,9 @@ import PreviewButton from './PreviewButton';
 import { ArrowRightIcon } from 'mdi-react';
 import CreateClipModal from './CreateClipModal';
 import ShareButton from './ShareButton';
+import { animated, useSpring } from 'react-spring';
 import ShowHideClipOptions from './ShowHideClipOptions';
+import useResizeObserver from 'src/hooks/useResizeObserver';
 
 type EpisodePlayerClipOptionsProps = {
   start: Maybe<number>;
@@ -24,6 +26,9 @@ type EpisodePlayerClipOptionsProps = {
 };
 
 const styles = {
+  container: css({
+    overflow: 'hidden',
+  }),
   buttonsContainer: css({
     '& > :not(:last-child)': {
       marginRight: 16,
@@ -47,7 +52,9 @@ const styles = {
   }),
 };
 
-const EpisodePlayerClipOptions: React.FC<EpisodePlayerClipOptionsProps> = ({
+const EpisodePlayerClipOptionsContent: React.FC<
+  EpisodePlayerClipOptionsProps & { isOpen: boolean }
+> = ({
   end,
   start,
   duration,
@@ -57,50 +64,62 @@ const EpisodePlayerClipOptions: React.FC<EpisodePlayerClipOptionsProps> = ({
   handleSetStart,
   previewing,
   time,
+  isOpen,
 }) => {
   const clipBoundsSet = !isNil(start) && !isNil(end) && end > start;
+  const {
+    ref,
+    dimensions: { height },
+  } = useResizeObserver<HTMLDivElement>();
+  const style = useSpring({ maxHeight: isOpen ? height : 0 });
 
+  return (
+    <animated.div className={styles.container} style={style}>
+      <div ref={ref}>
+        <ClipPreview start={start} end={end} time={time} length={duration} />
+        <div className={styles.timespanContainer}>
+          <TimeRecorder
+            currentTime={time}
+            time={start}
+            handleRecordClick={handleSetStart}
+            placeholder="Start time"
+          />
+          <ArrowRightIcon className={styles.timespanRangeIcon} size={16} />
+          <TimeRecorder
+            currentTime={time}
+            time={end}
+            handleRecordClick={handleSetEnd}
+            placeholder="End time"
+          />
+        </div>
+        <div className={styles.buttonsContainer}>
+          <PreviewButton
+            active={clipBoundsSet}
+            onClick={previewing ? handlePreviewStop : handlePreviewStart}
+            previewing={previewing}
+          />
+          <Show>
+            {({ isOpen: modalIsOpen, toggle: toggleModal }) => (
+              <>
+                {modalIsOpen && clipBoundsSet && (
+                  <CreateClipModal start={start!} end={end!} handleClose={toggleModal} />
+                )}
+                <ShareButton active={clipBoundsSet} onClick={toggleModal} />
+              </>
+            )}
+          </Show>
+        </div>
+      </div>
+    </animated.div>
+  );
+};
+
+const EpisodePlayerClipOptions: React.FC<EpisodePlayerClipOptionsProps> = props => {
   return (
     <Show>
       {({ isOpen, toggle }) => (
         <>
-          {isOpen && (
-            <>
-              <ClipPreview start={start} end={end} time={time} length={duration} />
-              <div className={styles.timespanContainer}>
-                <TimeRecorder
-                  currentTime={time}
-                  time={start}
-                  handleRecordClick={handleSetStart}
-                  placeholder="Start time"
-                />
-                <ArrowRightIcon className={styles.timespanRangeIcon} size={16} />
-                <TimeRecorder
-                  currentTime={time}
-                  time={end}
-                  handleRecordClick={handleSetEnd}
-                  placeholder="End time"
-                />
-              </div>
-              <div className={styles.buttonsContainer}>
-                <PreviewButton
-                  active={clipBoundsSet}
-                  onClick={previewing ? handlePreviewStop : handlePreviewStart}
-                  previewing={previewing}
-                />
-                <Show>
-                  {({ isOpen: modalIsOpen, toggle: toggleModal }) => (
-                    <>
-                      {modalIsOpen && clipBoundsSet && (
-                        <CreateClipModal start={start!} end={end!} handleClose={toggleModal} />
-                      )}
-                      <ShareButton active={clipBoundsSet} onClick={toggleModal} />
-                    </>
-                  )}
-                </Show>
-              </div>
-            </>
-          )}
+          <EpisodePlayerClipOptionsContent {...props} isOpen={isOpen} />
           <ShowHideClipOptions onClick={toggle} show={isOpen} />
         </>
       )}
